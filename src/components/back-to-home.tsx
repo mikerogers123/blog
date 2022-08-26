f4b2d1be0fcc37f1f316b1a7c3e89f5ebff19ec2
Michael <michael.rogers@justeattakeaway.com>
2022-08-26 14:25:43 +0100

    List blog posts on home page (orphaned links)

---
+++ src/components/back-to-home.tsx
@@ -0,0 +1,10 @@
+import { Link } from "gatsby";
+import React from "react";
+
+export const backToHomeLink = () => {
+    return (
+      <Link to="/">
+        Back to home
+      </Link>
+    );
+}
---
+++ src/components/posts.tsx
@@ -0,0 +1,43 @@
+import { graphql, Link, useStaticQuery } from "gatsby";
+import React from "react";
+
+type Post = {
+  thumbnail: string,
+  slug: string,
+  title: string,
+  id: string,
+  date: string
+}
+
+export const posts = () => {
+  
+  const data = useStaticQuery(graphql`
+    query AllMediumFeed {
+      allMediumFeed {
+        nodes {
+          thumbnail
+          slug
+          title
+          id
+          date
+        }
+      }
+    }
+  `);
+  
+  const posts = data.allMediumFeed.nodes as Array<Post>;
+  console.log(posts)
+  
+  return (
+    <ul>
+      {posts.map(post => (
+          <li key={post.id}>
+            <Link to={`/${post.id}`} key={post.id}>
+              <header>{post.title}</header>
+            </Link>
+            <div>Posted on {post.date}</div>
+          </li>
+        ))}
+    </ul>
+  );
+}
