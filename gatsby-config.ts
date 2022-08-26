61034772fb3b158928d668ad8b9643127fa73303
Michael <michael.rogers@justeattakeaway.com>
2022-08-26 11:12:07 +0100

    Add gatsby-source-medium-feed plugin to retreive all medium posts for @mikerogers1357

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,7 +9,16 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-sass"]
+  plugins: [
+    "gatsby-plugin-sass",
+    {
+      resolve: 'gatsby-source-medium-feed',
+      options: {
+        userName: '@mikerogers1357', // Medium user name
+        name: 'MediumFeed', // GraphQL query AllMediumFeed
+      },
+    }
+  ]
 };
 
 export default config;
