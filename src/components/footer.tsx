69bf32890ab288a6f2e1544087535c80ec9c9224
Michael <michael.rogers@justeattakeaway.com>
2022-09-01 10:57:46 +0100

    Add GatsbyJS/TerminalCSS links to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,9 +3,8 @@ import React from "react";
 export default function Footer() {
     return (
         <div>
-            <br></br>
-            <br></br>
-            <br></br>
+            <hr></hr>
+            Made with <a href="https://www.gatsbyjs.com/docs/">GatsbyJS</a>, styled with <a href="https://terminalcss.xyz/">Terminal CSS</a>
             <a className="right" href="https://github.com/mikerogers123/blog">Version {process.env.VERSION}</a>
         </div>
     );
