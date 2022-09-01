ffeaf65606c7ea5ca20255a2facc65885049950b
Michael <michael.rogers@justeattakeaway.com>
2022-09-01 12:04:33 +0100

    Fix src code link in footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,11 +1,27 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCode } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faCode);
 
 export default function Footer() {
-    return (
-        <div>
-            <hr></hr>
-            Made with <a href="https://www.gatsbyjs.com/docs/">GatsbyJS</a>, styled with <a href="https://terminalcss.xyz/">Terminal CSS</a>
-            <a className="right" href="https://github.com/mikerogers123/blog">Version {process.env.VERSION}</a>
-        </div>
-    );
+  return (
+    <div>
+      <hr></hr>
+      <p>
+        Made with <a href="https://www.gatsbyjs.com/docs/">GatsbyJS</a>, styled
+        with <a href="https://terminalcss.xyz/">Terminal CSS</a>
+      </p>
+      <button
+        className="btn btn-primary right"
+        onClick={(e) => {
+          e.preventDefault();
+          window.location.href = "https://github.com/mikerogers123/blog";
+        }}
+      >
+        <FontAwesomeIcon icon="code" />
+      </button>
+    </div>
+  );
 }
