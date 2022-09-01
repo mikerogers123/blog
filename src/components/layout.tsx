27464f87aa3c77dea66d47f5f6284c57607ee805
Michael <michael.rogers@justeattakeaway.com>
2022-09-01 10:53:56 +0100

    Add GitHub/LinkedIn links

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,28 +1,12 @@
-import { Link } from "gatsby";
 import React from "react";
 import Footer from "./footer";
-import ThemeSelector from "./theme-selector";
+import Navbar from "./navbar";
 
 export default function Layout({ children }: any) {
   return (
     <main>
       <div className="container">
-        <div className="terminal-nav">
-          <header className="terminal-logo">
-            <div className="logo terminal-prompt">
-              <Link to="/" className="no-style">
-                Mike Rogers' Blog
-              </Link>
-            </div>
-          </header>
-          <nav className="terminal-menu">
-            <ul vocab="https://schema.org/" typeof="BreadcrumbList">
-              <li>
-                <ThemeSelector></ThemeSelector>
-              </li>
-            </ul>
-          </nav>
-        </div>
+        <Navbar></Navbar>
       </div>
       <div className="container">{children}</div>
       <div className="container">
---
+++ src/components/navbar.tsx
@@ -0,0 +1,44 @@
+import { Link } from "gatsby";
+import React from "react";
+import ThemeSelector from "./theme-selector";
+
+export default function Navbar() {
+  return (
+    <div className="terminal-nav">
+      <header className="terminal-logo">
+        <div className="logo terminal-prompt">
+          <Link to="/" className="no-style">
+            Mike Rogers' Blog
+          </Link>
+        </div>
+      </header>
+      <nav className="terminal-menu">
+        <ul vocab="https://schema.org/" typeof="BreadcrumbList">
+          <li property="itemListElement" typeof="ListItem">
+            <a
+              href="https://www.linkedin.com/in/michael-james-rogers"
+              property="item"
+              typeof="WebPage"
+              className="menu-item"
+            >
+              <span property="name">LinkedIn</span>
+            </a>
+          </li>
+          <li property="itemListElement" typeof="ListItem">
+            <a
+              href="https://github.com/mikerogers123"
+              property="item"
+              typeof="WebPage"
+              className="menu-item"
+            >
+              <span property="name">GitHub</span>
+            </a>
+          </li>
+          <li>
+            <ThemeSelector></ThemeSelector>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
